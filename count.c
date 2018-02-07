5242dfd2ec1e2d1c1b647dd87319b5686959f147
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-07 16:06:35 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char a[100]="laptop is good";
+	int b=0;
+	for(int i=0;a[i]!='\0';i++)
+	{
+		if(((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))&&(a[i]!=' '))
+		{
+			b++;
+		}
+	}
+	printf("%d",b);
+  return 0;
+}
