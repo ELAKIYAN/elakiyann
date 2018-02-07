9e128075164d9297db2889c8249754fb3054e2f8
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-07 16:40:57 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf(" enter the values of a&b:");
+scanf("%d %d",&a,&b);
+printf(" after swap:");
+a=a+b;
+b=a-b:
+a=a-b;
+printf("%d,%d",a,b);
+getch();
+}
