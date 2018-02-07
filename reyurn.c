86bd8473f2eb8f65ceca5bede1d861da77ac767a
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-07 16:01:43 +0530

    Create reyurn.c

---
+++ reyurn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int hrs1,hrs2,mins1,mins2,m,n;
+printf("Enter the hrs1 and mins1:");
+scanf("%d%d",&hrs,&mins);
+printf("Enter the hrs2 and mins2:");
+scanf("%d%d",&hrs,&mins);
+m=hrs1-hrs2;
+n=mins1-mins2;
+printf("%d %d",m,n);
+
+}
