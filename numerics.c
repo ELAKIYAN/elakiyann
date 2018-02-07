af5e32646349ad960af852a8252f15dae645b3ff
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-07 16:38:06 +0530

    Create numerics.c

---
+++ numerics.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+ char str[100];
+ int dig,j;
+ dig=j=0;
+ printf("Enter any string :");
+ gets(str);
+  while(str[j]!='\0')
+ {
+ if(str[j]>='0' && str[j]<='9')
+  {
+  dig++;
+  }
+  j++;
+  }
+  printf("Digits = %d\n", dig);
+
+}
